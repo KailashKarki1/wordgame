8f87745f7c3c07a307a6bcd6d77b67d39ea90747
kailash <sandipkarki805@gmail.com~>
2024-10-08 16:10:57 -0400

    v1

---
+++ src/main/java/com/example/spring_word_guess/SpringWordGuessApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_word_guess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWordGuessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWordGuessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_word_guess/SpringWordGuessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_word_guess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWordGuessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
